e2cd65676fb4deb5d642339551d374a3e9a95744
vlad <vlad@debian.debian>
2020-01-05 01:26:11 +0600

    who we are block

---
+++ src/components/MainSection/components/UserLittleInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="user-little-info">
+    <div class="image">
+      <img :src="userLittleInfo.imageUrl" alt="user-image">
+    </div>
+    <div class="name"> {{ userLittleInfo.fullName }} </div>
+    <div class="jobPosition"> {{ jobPosition }} </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import { IUserLittleInfo } from '@/Interfaces'
+
+@Component
+export default class UserLittleInfo extends Vue {
+    @Prop()
+    userLittleInfo!: IUserLittleInfo;
+
+    get jobPosition () : String {
+      return `${this.userLittleInfo.jobPosition.field} ${this.userLittleInfo.jobPosition.position}`
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  .user-little-info {
+    display: flex;
+    flex-direction: column;
+    padding: 3rem;
+    text-align: center;
+    .image {
+      img {
+        border-radius: 50%;
+      }
+    }
+    .name {
+      color: $color-dune;
+      font-weight: bold;
+      font-size: 26px;
+      padding-bottom: 0.7rem;
+    }
+
+  }
+
+</style>
---
+++ src/components/MainSection/components/WhoWeAreBlock.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="who-we-are-block">
+      <h4>{{ headerTitle }}</h4>
+      <div class="row">
+          <div class="creative-text">
+              <h2>
+                  {{ headerLevelTwoTitle.nonStrongPart }}
+                  <strong> {{ headerLevelTwoTitle.strongPart }} </strong>
+              </h2>
+              <p>{{ paragraphText }}</p>
+          </div>
+          <div class="creative-pic">
+              <img :src="creativeImage" alt="creativity-pic">
+          </div>
+      </div>
+      <div class="row users-images">
+          <UserLitteInfo v-for="(userLittleInfo, index) in userInfo" :key="index" :userLittleInfo="userLittleInfo" />
+      </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import UserLitteInfo from '@/components/MainSection/components/UserLittleInfo'
+import { IUserLittleInfo } from '@/Interfaces'
+
+interface IDataFnReturnInterface {
+    headerTitle: String,
+    headerLevelTwoTitle : {
+        nonStrongPart: String
+        strongPart: String
+    },
+    paragraphText: String,
+    creativeImage: any,
+    userInfo: IUserLittleInfo[]
+
+}
+
+@Component({
+  components: {
+    UserLitteInfo
+  }
+})
+export default class WhoWeAreBlock extends Vue {
+  data () : IDataFnReturnInterface {
+    return {
+      headerTitle: 'Who We Are',
+      headerLevelTwoTitle: {
+        nonStrongPart: 'We live with',
+        strongPart: 'creativity'
+      },
+      paragraphText: `
+      Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores.
+      `,
+      creativeImage: require('@/assets/images/creativity.png'),
+      userInfo: [{
+        name: 'John',
+        secondName: '',
+        fullName: 'John Doe',
+        jobPosition: {
+          field: 'Art',
+          position: 'director'
+        },
+        imageUrl: require('@/assets/images/people/ArtDirector.jpg')
+      },
+      {
+        name: 'Mike',
+        secondName: '',
+        fullName: 'Mike Doe',
+        jobPosition: {
+          field: 'Web',
+          position: 'developer'
+        },
+        imageUrl: require('@/assets/images/people/WebDeveloper.jpg')
+      },
+      {
+        name: 'Neil',
+        secondName: '',
+        fullName: 'Neil Doe',
+        jobPosition: {
+          field: 'Web',
+          position: 'designer'
+        },
+        imageUrl: require('@/assets/images/people/WebDesigner.jpg')
+      },
+      {
+        name: 'Mark',
+        secondName: '',
+        fullName: 'Mark Joe',
+        jobPosition: {
+          field: 'UI',
+          position: 'designer'
+        },
+        imageUrl: require('@/assets/images/people/UIDesigner.jpg')
+      },
+      {
+        name: 'Stephen',
+        secondName: '',
+        fullName: 'Stephen B',
+        jobPosition: {
+          field: 'Digital',
+          position: 'imaging'
+        },
+        imageUrl: require('@/assets/images/people/DigitalImaging.jpg')
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.who-we-are-block {
+    padding: 2rem 0 2rem 0;
+    width: 100%;
+    background: $color-concrete-solid;
+    .row {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .creative-text {
+            max-width: 20rem;
+        }
+    }
+    h4 {
+        text-align: center;
+        font-size: 46px;
+    }
+    .creative-text {
+        h2 {
+            font-weight: 100;
+            font-size: 30px;
+        }
+        p {
+            font-size: 16px;
+        }
+    }
+}
+</style>
