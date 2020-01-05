cf69bca274b89b77ea1db9f37dff0f97fba74462
vlad <vlad@debian.debian>
2020-01-05 13:13:20 +0600

    our blog block added

---
+++ src/components/MainSection/components/OurBlog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="our-blog-block">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'our-blog-block'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
