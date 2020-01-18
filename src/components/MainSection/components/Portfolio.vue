17b5fc22b91598f89a07b793c9442632da0fd6ca
vlad <vlad@debian.f13>
2020-01-18 04:22:28 -0500

    overlayed portfolio works

--- src/components/MainSection/components/Portfolio.vue
+++ src/components/MainSection/components/Portfolio.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="portfolio-work">
-    <img :src="portfolio.image" alt="pic for portfolio image">
-  </div>
+  <div @mouseenter="onMouseEnter" @mouseleave="onMouseOut">
+    <div :class="['right-geometry', 'overlay', 'transitioned', { 'overlay-is-active' : hovered }]" >
+      <div class="portfolio-name">Portfolio name</div>
+      <i class="icon-plus fas fa-plus"></i>
+    </div>
+    <div :class="['right-geometry', 'portfolio-work', 'transitioned', { blurred: hovered }]">
+      <img :src="portfolio.image" alt="pic for portfolio image">
+    </div></div>
 </template>
 
 <script>
@@ -19,17 +24,66 @@ export default {
         required: true
       }
     }
+  },
+  data () {
+    return {
+      hovered: false
+    }
+  },
+  methods: {
+    onMouseEnter () {
+      console.log(1)
+      this.hovered = true
+    },
+    onMouseOut () {
+      console.log(2)
+      this.hovered = false
+    }
   }
 
 }
 </script>
 
 <style lang="scss" scoped>
+  .transitioned {
+    transition: all 0.3s ease-in-out;
+  }
+  .blurred {
+    filter: blur(8px);
+  }
+
+  .right-geometry {
+    height: 15rem;
+    width: 20rem;
+    margin: 0.5rem;
+  }
+  .overlay {
+    position: absolute;
+    transform: scale(0);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: rgba($color-mountain-meadow, 0.3);
+    font-size: 30px;
+    color: $color-white-solid;
+    .portfolio-name {
+      margin: 1.5rem;
+    }
+
+  }
+  .overlay-is-active {
+    z-index: 1;
+    transform: scale(1);
+
+  }
   .portfolio-work {
     display: flex;
     justify-content: stretch;
     img {
-      max-width: 15rem;
+      width: 20rem;
+      height: 15rem;
     }
   }
 </style>
